e83e644d698245fd68f8330507a0b030fe1ea75d
TheSUPERCD <supercdeb2000@gmail.com>
2020-02-22 15:11:09 +0530

    added nessesary files

---
+++ IRvisualizer.py
@@ -0,0 +1,14 @@
+import cv2
+import sys
+import numpy as np
+
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+else:
+    print("Please provide a filename !")
+    sys.exit(1)
+
+irImg = cv2.imread('ImgSat\\' + filename +'Infrared.png', 0)
+
+heatMap = cv2.applyColorMap(irImg, cv2.COLORMAP_JET)
+cv2.imwrite('HeatMaps\\' + filename + 'HeatMap.png', heatMap)
